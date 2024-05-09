b0528d7a2e34d1caab7314222e66d3cb7eaac0f7
Pasindu Upeka <upekasilva2000@gmail.com>
2024-05-09 20:06:40 +0530

    BookedRoom class compelted

--- src/main/java/com/myproject/stayeaseBookingApp/model/Room.java
+++ src/main/java/com/myproject/stayeaseBookingApp/model/Room.java
@@ -1,16 +1,24 @@
 package com.myproject.stayeaseBookingApp.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
+@Entity
+@Data
 public class Room {
 
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String roomType;
     private BigDecimal roomPrice;
     private boolean isBooked=false;
+
+    @OneToMany(fetch = FetchType.LAZY,cascade = CascadeType.ALL)
     private List<BookedRoom> bookings;
 
 
