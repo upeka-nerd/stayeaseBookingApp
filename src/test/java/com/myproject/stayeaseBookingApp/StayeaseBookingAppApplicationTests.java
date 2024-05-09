c07330966d6515a3f8227df5b0413a7df55ceb63
Pasindu Upeka <upekasilva2000@gmail.com>
2024-05-09 16:24:13 +0530

    Initial commit

---
+++ src/test/java/com/myproject/stayeaseBookingApp/StayeaseBookingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myproject.stayeaseBookingApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StayeaseBookingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
