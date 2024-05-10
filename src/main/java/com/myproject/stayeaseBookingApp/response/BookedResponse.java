789b72852940d65de7d1c3062da9145dfba073b3
Pasindu Upeka <upekasilva2000@gmail.com>
2024-05-10 14:24:23 +0530

    server config

--- src/main/java/com/myproject/stayeaseBookingApp/response/BookedResponse.java
+++ src/main/java/com/myproject/stayeaseBookingApp/response/BookedResponse.java
@@ -1,4 +1,58 @@
 package com.myproject.stayeaseBookingApp.response;
 
+import com.myproject.stayeaseBookingApp.model.Room;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
 public class BookedResponse {
+
+
+    private long bookingId;
+
+    private LocalDate checkInDate;
+
+    private LocalDate checkOutDate;
+
+    private String guestFullName;
+
+    private String guestEmail;
+
+    private int numberOfAdults;
+
+    private int numberOfChildrens;
+
+    private int totalNumberOfGuests;
+
+    private String bookingConfirmationCode;
+    private Room room;
+
+
+    public BookedResponse(long bookingId, LocalDate checkInDate,
+                          LocalDate checkOutDate, String bookingConfirmationCode) {
+        this.bookingId = bookingId;
+        this.checkInDate = checkInDate;
+        this.checkOutDate = checkOutDate;
+        this.bookingConfirmationCode = bookingConfirmationCode;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
 }
--- src/main/java/com/myproject/stayeaseBookingApp/response/RoomResponse.java
+++ src/main/java/com/myproject/stayeaseBookingApp/response/RoomResponse.java
@@ -1,13 +1,46 @@
 package com.myproject.stayeaseBookingApp.response;
 
+import com.myproject.stayeaseBookingApp.model.BookedRoom;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.apache.tomcat.util.codec.binary.Base64;
+
+import java.math.BigDecimal;
+import java.sql.Blob;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class RoomResponse {
 
 
 
+    private long id;
+    private String roomType;
+    private BigDecimal roomPrice;
+    private boolean isBooked;
+    private String photos;
+
+    private List<BookedResponse>bookings;
 
 
+    public RoomResponse(long id, String roomType, BigDecimal roomPrice) {
+        this.id = id;
+        this.roomType = roomType;
+        this.roomPrice = roomPrice;
+    }
 
-
-
-
+    public RoomResponse(long id, String roomType,
+                        BigDecimal roomPrice, boolean isBooked,
+                        byte[] photoByte, List<BookedResponse> bookings) {
+        this.id = id;
+        this.roomType = roomType;
+        this.roomPrice = roomPrice;
+        this.isBooked = isBooked;
+        this.photos = photoByte ==null? Base64.encodeBase64String(photoByte):null;
+        this.bookings = bookings;
+    }
 }
